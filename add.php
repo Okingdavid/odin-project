fb7fb5ae39885b3b2be4a02e3c568160a65c51d8
Okingdavid <89081864+Okingdavid@users.noreply.github.com>
2022-02-19 14:31:31 +0100

    Add files via upload

---
+++ add.php
@@ -0,0 +1,3 @@
+<php 
+include(C:\Users\USER\Documents\project2\mainproject\public\project2.php);
+?>
---
+++ login.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>login</title>
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+     <div class="global-container  opacity-80">
+
+          <div>
+               <!-- navigation start here -->
+               <nav>
+                    <!-- the navigation brand start here thanks -->
+                    <div class="nav-brand">
+                         <a class="flex ml-44" href="#"><img class="logo w-14 mt-4" src="C:\Users\USER\Documents\project2\mainproject\public\img-src\odin-logo-bd86cf893a3de1f1daceabc1377f58669776616a91ab70c601fd5c16a4686468 (1).svg" alt="">
+                              <div class="logo-text font-bold text-2xl uppercase text-yellow-600 pt-9"> The Odin Project </div>
+
+                         </a>
+                    </div>
+                      <!-- the navigation brand ends here -->
+
+                     <!-- the navigation link items start here thanks -->
+                   <div class="text-gray-400 font-bold text- hidden md:inline-block">
+                    <ul class="nav-item flex justify-end  absolute top-10 left-1/2 ">
+                         <li><a class="m-2 hover:text-gray-900" href="#">ALL Paths</a></li>
+                         <li><a class="m-2 hover:text-gray-900" href="#">Community</a></li>
+                         <li><a class="m-2 hover:text-gray-900" href="#">About</a></li>
+                         <li><a class="m-2 hover:text-gray-900" href="#">FAQ</a></li>
+                         <li class="font-bold  text-gray-900">|</li>
+                         <li><a class="m-2  hover:text-gray-900" href="C:\Users\USER\Documents\project2\mainproject\public\sign-up.html">Sign up</a></li>
+                         <li><a class="m-2 hover:text-gray-900 text-black" href="#">Log In</a></li>
+                    </ul>
+                   </div>
+                   <!-- the navigation link items end here -->
+               </nav>
+
+               <div>
+                    <p class="text-center text-4xl pt-9 text-yellow-700 font-light">log In</p>
+               </div>
+
+               <div class="min-h-screen flex items-center justify-center ">
+
+                    <div class="">
+
+                         <form action="" class="bg-white p-60  relative pr-96  rounded shadow-2xl w-60">
+
+                             
+                              <!-- <div class="bg-gray-100 rounded  absolute left-14 top-14 ">
+
+                                   <label for=""><svg class="w-6 relative top-10 left-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg></label>
+                                   <input type="text" id="name" name="name" placeholder="username" class="flex-1 p-4  pr-72 bg-transparent  border-gray-600 text-center placeholder-purple-300 outline-none text-blue-300">
+                              </div> -->
+
+                              <div class="bg-gray-100 rounded absolute left-14 top-14">
+                                   <label for=""><svg class="w-6 relative top-10 left-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg></label>
+                                   <input type="text" id="email" name="email" placeholder="Email" class="flex-1 p-4 pr-72 bg-transparent border-gray-600 outline-none text-center">
+                              </div> 
+
+                              <div class="bg-gray-100 rounded absolute left-14 top-36">
+                                   <label for=""><svg class="w-6 relative top-10 left-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clip-rule="evenodd"></path><path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"></path></svg>
+                                   </label>
+                                   <input type="text" id="password" name="password" placeholder="password" class="flex-1 p-4 pr-72 bg-transparent border-gray-600 outline-none text-center">
+                              </div>
+
+                             
+
+
+                               
+                               <div class="absolute left-9 top-64 text-sm">
+                                   <input type="checkbox" id="sign-up">
+                                   <label for="sign-up">By signing up, you agree to our terms of use.</label>
+                              </div> 
+
+                              <div class="hero-section-button pt-52 text-center absolute right-32 top-24">
+                                   <button class="bg-blue-400 hover:bg-blue-300 p-3 text-white font-bold text-lg rounded-lg w-96 shadow-xl uppercase">login</button>
+                              </div>
+
+                              <div class="hero-section-button   pt- text-center absolute right-32 top-72  mt-24">
+                                   <button class="bg-yellow-600 hover:bg-yellow-300 p-3 text-white font-bold text-lg rounded-lg w-96 shadow-xl">TRY GITHUB </button>
+                              </div>
+ 
+                             
+
+                         </form>
+
+                          
+
+                    </div>
+
+               </div>
+
+     </div>
+</body>
+</html>
